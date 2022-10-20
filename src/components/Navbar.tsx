7d1a2becbabeb7c9ee25d8a2e1298298f9e471f8
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-10-20 13:32:24 +0700

    feat(components): create navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,132 @@
+import { Transition } from "@headlessui/react";
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const navs = [
+    { name: "Dashboard", path: "/dashboard" },
+    { name: "Petugas", path: "/a" },
+    { name: "Kelas", path: "/e" },
+    { name: "Siswa", path: "/y" },
+    { name: "Pembayaran", path: "/s" },
+    { name: "Reports", path: "/x" },
+  ];
+  const navClass =
+    "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium";
+  const activeNavClass = " text-white bg-slate-900";
+  const mobileNavClass = " block";
+
+  return (
+    <div>
+      <nav className="bg-slate-800 shadow">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <Link to={"/"} className="flex items-center gap-2 flex-shrink-0">
+                <img
+                  src="https://img.icons8.com/ios-glyphs/90/ffffff/money-transfer.png"
+                  width={40}
+                  height={40}
+                />
+                <h1 className="text-white font-bold">EDUsys</h1>
+              </Link>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  {navs.map((item, index) => (
+                    <NavLink
+                      key={index}
+                      to={item.path}
+                      className={({ isActive }) => {
+                        return isActive ? navClass + activeNavClass : navClass;
+                      }}
+                    >
+                      {item.name}
+                    </NavLink>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                {navs.map((item, index) => (
+                  <NavLink
+                    key={index}
+                    to={item.path}
+                    className={({ isActive }) =>
+                      isActive
+                        ? navClass + activeNavClass + mobileNavClass
+                        : navClass + mobileNavClass
+                    }
+                  >
+                    {item.name}
+                  </NavLink>
+                ))}
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
