6e576a23f3dcf1f572eadd8ae118d172a8521996
Hifdzi Maulana <hifdzimaulana12@gmail.com>
2022-11-10 20:45:43 +0700

    fix(Siswa.page): fetching data siswa from backend API

--- src/pages/Siswa.page.tsx
+++ src/pages/Siswa.page.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import { Navbar } from "../components";
+import siswaService from "../services/siswa.service";
+import IPagination from "../types/pagination.type";
+import ISiswaData from "../types/siswa.type";
 
 function SiswaPage() {
+  const [dataSiswa, setDataSiswa] = useState<IPagination<ISiswaData>>();
+  useEffect(() => {
+    siswaService.getAll().then((response) => setDataSiswa(response.data));
+  }, []);
+
   return (
     <div className="bg-slate-100 w-screen h-screen">
       <Navbar />
       Halo ini halaman siswa
+      <div>
+        {dataSiswa?.rows.map((val, key) => {
+          return (
+            <div key={key}>
+              {val.nama} - {val.alamat}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
